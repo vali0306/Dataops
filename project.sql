f0d43d63fb4c272e276c9d1a08bff85678c9ada9
valisathishvangala <valisathishreddyms@gmail.com>
2021-11-18 17:52:46 +0000

    Added New Table

---
+++ project.sql
@@ -0,0 +1,8 @@
+s.no                        Product                               Price                     Prod Desc
+1                            Laptop                                45000                   Computers
+
+2                            Pencils                                 2                            Stationery
+
+3                            Rice                                     64.45                    Groceries
+
+4                            Furniture                             65000                   Interiors
